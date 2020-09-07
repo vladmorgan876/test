d1f5c7ab98de7a30a4ca556fac5f0ca662f64f8e
vlad <morhunov.volodymyr@gmail.com>
2020-09-07 13:19:38 +0300

    index page

--- index.php
+++ index.php
@@ -5,17 +5,21 @@
     <meta name="viewport">
     <title>test</title>
     <link rel="stylesheet" href="style.css">
+    <a href="">ссылка на GITHUB/commit</a>
 </head>
 <body>
 <h1>Hello, NIX Education</h1>
-</body>
-</html>
+
 <?php
 echo '<button class="button1"><a  href="table1.php">урок 1</a></button><br>';
-echo '<button  class="button1"><a  href="#">урок 2</a></button><br>';
+echo '<button  class="button1"><a  href="table2.php">урок 2</a></button><br>';
 echo '<button  class="button1"><a  href="str.php">урок 3</a></button>';
 
-
+echo '<footer>';
+echo ' <a class="link" href="https://github.com/vladmorgan876/test.git">ссылка на репозиторий GitHub</a></p>';
+echo '</footer>';
+echo '</body>';
+echo '</html>';
 
 
 
