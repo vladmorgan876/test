a06136fbb9b6de52dea206bea87019c86155b3f9
vlad <morhunov.volodymyr@gmail.com>
2020-09-07 21:58:08 +0300

    commit 2 table 2

--- table2.php
+++ table2.php
@@ -24,7 +24,6 @@ for ($a=1;$a<=5;$a++){
     $mult1=$mult[0];
     $mult2=$mult[1];
         $col=[1=>'red',2=>'green',3=>'yellow',4=>'blue',5=>'cyan',6=>'magenta',7=>'orchid',8=>'Tomato',9=>'GreenYellow',0=>'SandyBrown'];
-        $search_array = array('first' => 1, 'second' => 4);
         if (array_key_exists($a, $col)) {
             if (array_key_exists($i, $col)){
                 echo "<span style='color: $col[$a]'>".$a.'</span>'.'*'."<span style='color: $col[$i]'>".$i.'</span>>'.'='."<span style='color: $col[$mult1]'>".$mult1.'</span>'."<span style='color: $col[$mult2]'>".$mult2.'</span>'.'<br>';
@@ -42,8 +41,18 @@ for ($a=6;$a<=10;$a++){
         $mult=(string)$mult;
         $mult1=$mult[0];
         $mult2=$mult[1];
-        $col=[1=>'red',2=>'green',3=>'yellow',4=>'blue',5=>'cyan',6=>'magenta',7=>'orchid',8=>'Tomato',9=>'GreenYellow',0=>'SandyBrown',10=>'Pink'];
-        $search_array = array('first' => 1, 'second' => 4);
+
+        $col=[1=>'red',2=>'green',3=>'yellow',4=>'blue',5=>'cyan',6=>'magenta',7=>'orchid',8=>'Tomato',9=>'GreenYellow',0=>'SandyBrown'];
+        if($a>9){
+            $a=(string)$a;
+            $a1=$a[0];
+            $a2=$a[1];
+            if (array_key_exists($a1, $col)) {
+                if (array_key_exists($i, $col)){
+                    echo "<span style='color: $col[$a1]'>".$a1.'</span>'."<span style='color: $col[$a2]'>".$a2.'</span>'.'*'."<span style='color: $col[$i]'>".$i.'</span>>'.'='."<span style='color: $col[$mult1]'>".$mult1.'</span>'."<span style='color: $col[$mult2]'>".$mult2.'</span>'.'<br>';
+                }
+            }
+        }
         if (array_key_exists($a, $col)) {
             if (array_key_exists($i, $col)){
                 echo "<span style='color: $col[$a]'>".$a.'</span>'.'*'."<span style='color: $col[$i]'>".$i.'</span>>'.'='."<span style='color: $col[$mult1]'>".$mult1.'</span>'."<span style='color: $col[$mult2]'>".$mult2.'</span>'.'<br>';
