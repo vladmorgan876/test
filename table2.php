2dc2c2ca1fd6ef654d5dcb64742ce3bd8216a326
vlad <morhunov.volodymyr@gmail.com>
2020-09-07 20:25:40 +0300

    commit table 2

--- table2.php
+++ table2.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport">
-    <title>table 1</title>
+    <title>table 2</title>
     <link rel="stylesheet" href="style.css">
-    <a href="index.php">на главную</a>
+    <p style="font-size:20px"><a href="index.php">на главную</a></p>
+    <a href="">ссылка на GITHUB/commit</a>
 </head>
 <body>
 
@@ -16,20 +17,20 @@
 echo '<table>';
 echo '<tr>';
 for ($a=1;$a<=5;$a++){
-
-        echo '<td>';
-        for($i=1;$i<=10;$i++){
-            if($a==1||$i==1){
-                echo '<span style="color: red">'. $a.'</span>'.'*'.$i.'='.$a*$i.'<br>';
+    echo '<td>';
+    for($i=1;$i<=10;$i++){
+    $mult=$a*$i;
+    $mult=(string)$mult;
+    $mult1=$mult[0];
+    $mult2=$mult[1];
+        $col=[1=>'red',2=>'green',3=>'yellow',4=>'blue',5=>'cyan',6=>'magenta',7=>'orchid',8=>'Tomato',9=>'GreenYellow',0=>'SandyBrown'];
+        $search_array = array('first' => 1, 'second' => 4);
+        if (array_key_exists($a, $col)) {
+            if (array_key_exists($i, $col)){
+                echo "<span style='color: $col[$a]'>".$a.'</span>'.'*'."<span style='color: $col[$i]'>".$i.'</span>>'.'='."<span style='color: $col[$mult1]'>".$mult1.'</span>'."<span style='color: $col[$mult2]'>".$mult2.'</span>'.'<br>';
             }
-            else{
-                echo $a.'*'.$i.'='.$a*$i.'<br>';
-            }
-
         }
-
-
-
+    }
 }
 echo '</td>';
 echo '</tr>';
@@ -37,11 +38,22 @@ echo '<tr>';
 for ($a=6;$a<=10;$a++){
     echo '<td>';
     for($i=1;$i<=10;$i++){
-        echo $a.'*'.$i.'='.$a*$i.'<br>';
+        $mult=$a*$i;
+        $mult=(string)$mult;
+        $mult1=$mult[0];
+        $mult2=$mult[1];
+        $col=[1=>'red',2=>'green',3=>'yellow',4=>'blue',5=>'cyan',6=>'magenta',7=>'orchid',8=>'Tomato',9=>'GreenYellow',0=>'SandyBrown',10=>'Pink'];
+        $search_array = array('first' => 1, 'second' => 4);
+        if (array_key_exists($a, $col)) {
+            if (array_key_exists($i, $col)){
+                echo "<span style='color: $col[$a]'>".$a.'</span>'.'*'."<span style='color: $col[$i]'>".$i.'</span>>'.'='."<span style='color: $col[$mult1]'>".$mult1.'</span>'."<span style='color: $col[$mult2]'>".$mult2.'</span>'.'<br>';
+            }
+        }
     }
 }
 echo '</td>';
 echo '</tr>';
+
 echo '</table>';
 
 
